05424fc1a60317fcbf634bf0a8d5d51b3d10720a
Tatyana Shuniborova <tashu529@gmail.com>
2024-03-14 00:35:05 +0300

    correct icons

---
+++ src/components/Icons/FooterIcons.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import fontawesome from '@fortawesome/fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
+function FooterIcons() {
+    // fontawesome.library.add(faPhone)
+    // const footer_icon = <FontAwesomeIcon icon="fa-solid fa-phone" style={{ color: "#63E6BE", }} /> 
+    
+  return (
+    // <FontAwesomeIcon icon="fa-solid fa-phone" style={{color: "#63E6BE",}} /> 
+  )
+}
+
+export default FooterIcons
